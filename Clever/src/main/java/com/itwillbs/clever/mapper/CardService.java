4483410f3a1aa2282253b274cdd641c2dc99915f
chanyoung <wsx6578@naver.com>
2023-05-15 15:49:39 +0900

    xml 파일 수정

---
+++ Clever/src/main/java/com/itwillbs/clever/mapper/CardService.java
@@ -0,0 +1,8 @@
+package com.itwillbs.clever.mapper;
+
+import org.springframework.stereotype.*;
+
+@Service
+public class CardService {
+
+}
---
+++ Clever/src/main/java/com/itwillbs/clever/service/CardMapper.java
@@ -0,0 +1,5 @@
+package com.itwillbs.clever.service;
+
+public class CardMapper {
+
+}
